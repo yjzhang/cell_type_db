a8bf2ea339d865a8fda1b8a103cbfbc91260d937
yjzhang <yjzhang@cs.washington.edu>
2018-05-24 16:23:19 -0700

    added rank correlation query test

--- test_rank_correlation_query.py
+++ test_rank_correlation_query.py
@@ -1,12 +1,11 @@
-# builds a nmslib index of all archs4 profiles...
-
-import pickle
+import time
 
 import nmslib
 import numpy as np
 import tables
 from scipy import sparse
 import scipy.io
+import scipy.stats
 from tqdm import tqdm
 
 filename = 'archs4/human_matrix.h5'
@@ -44,7 +43,7 @@ with open('test_data/10x_pooled_400_gene_names.tsv') as f:
     data_gene_names = [x.strip() for x in f.readlines()]
 
 
-# select the n genes with highest variance
+# select the 1000 genes with highest variance
 n_genes = 10000
 top_genes = max_variance_indices[:n_genes]
 top_gene_names = gene_names[top_genes]
@@ -63,73 +62,43 @@ for i, gene in enumerate(data_gene_names):
 for gene_name in overlapping_gene_names:
     db_gene_indices.append(db_gene_ids_map[gene_name])
     data_gene_indices.append(data_gene_ids_map[gene_name])
-num_genes = len(overlapping_gene_names)
-print('number of genes: ' + str(num_genes))
+print('number of genes: ' + str(len(overlapping_gene_names)))
 
-
-np.savetxt('data_gene_indices.txt', data_gene_indices, fmt='%d')
-np.savetxt('db_gene_indices.txt', db_gene_indices, fmt='%d')
-
-"""
-https://github.com/nmslib/nmslib/blob/master/manual/manual.pdf
-
-potential spaces:
-    cosinesimil (cosine similarity)
-    l1 (l1 distance)
-    l2
-    lp:p=...
-    linf
-    bit_hamming
-    jsdivslow, jsdivfast, jsdivfastapprox (Jensen-Shannon divergence)
-    kldivfast, kldivfastrq (KL divergence, right query)
-"""
-space = 'cosinesimil'
-index = nmslib.init(method='hnsw', space=space)
-
-for i in tqdm(range(expression_data.shape[0])):
-    row = expression_data[i, db_gene_indices].astype(np.float32)
-    row = row/row.sum()
-    index.addDataPoint(i, row)
-
-index.createIndex()
-index.saveIndex('archs4/nmslib_{0}_index_{1}_genes'.format(space, n_genes))
-
-# test some points
-print('test on data')
-correct_count = 0
-incorrect_count = 0
-for i in tqdm(range(expression_data.shape[0])):
-    row = expression_data[i, db_gene_indices].astype(np.float32)
-    row = row/row.sum()
-    ind, dist = index.knnQuery(row, 1)
-    if ind[0] == i:
-        correct_count += 1
-    else:
-        incorrect_count += 1
-print(correct_count, incorrect_count)
-
-# load index
-#index.loadIndex('archs4/nmslib_{0}_index_data_subset'.format(space))
-
-
+# linear scan using spearman correlation
 # test on test dataset
 labels = data_mat['labels'].flatten()
 for x in set(labels):
     print('label: ' + str(x))
+
     means = np.array(data[:,labels==x].mean(1)).flatten()
     means = means[data_gene_indices]
     means = means/means.sum()
-    ind, dist = index.knnQuery(means, 10)
+    max_corr = 0.0
+    best_id = 0
+    t0 = time.time()
+    for i in range(expression_data.shape[0]):
+        corr = scipy.stats.spearmanr(means, expression_data[i, db_gene_indices])[0]
+        if corr > max_corr:
+            max_corr = corr
+            best_id = i
     # get tissues corresponding to indices
-    print(ind, dist)
-    print([sample_tissues[i] for i in ind])
+    print(best_id, max_corr)
+    print(sample_tissues[best_id])
+    print('elapsed time: ' + str(time.time() - t0))
 
 # test on individual cells rather than cluster means
 for i, lab in enumerate(labels):
     print('label: ' + str(lab))
-    cell = data[:,i].toarray()
+    cell = data[:,i].toarray().flatten()
     cell = cell[data_gene_indices]
     cell = cell/cell.sum()
-    ind, dist = index.knnQuery(cell, 1)
-    print(ind, dist)
-    print([sample_tissues[i] for i in ind])
+    max_corr = 0.0
+    best_id = 0
+    for i in range(expression_data.shape[0]):
+        corr = scipy.stats.spearmanr(cell, expression_data[i, db_gene_indices])[0]
+        if corr > max_corr:
+            max_corr = corr
+            best_id = i
+    # get tissues corresponding to indices
+    print(best_id, max_corr)
+    print(sample_tissues[best_id])
